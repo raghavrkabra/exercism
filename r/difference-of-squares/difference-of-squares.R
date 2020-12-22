820fc40814646c4bdfbf78698690f6409db93345
Raghav Kabra <raghavrkabra@gmail.com>
2020-12-22 22:54:56 +0530

    Solved Difference of Squares exercises in R track

--- r/difference-of-squares/difference-of-squares.R
+++ r/difference-of-squares/difference-of-squares.R
@@ -1,5 +1,16 @@
+sum_of_n_squares <- function(number) {
+  return(number * (number + 1) * (number * 2 + 1) / 6)
+}
+
+
+sum_of_n <- function(number) {
+  return(number * (number + 1) / 2)
+}
+
+
 # this is a stub function that takes a natural_number
 # and should return the difference-of-squares as described
 # in the README.md
 difference_of_squares <- function(natural_number) {
+  return(abs(sum_of_n_squares(natural_number) - sum_of_n(natural_number)^2))
 }
