ffe2c065b58c498ed360f76b2803eef257ff23ac
Raghav Kabra <raghavrkabra@gmail.com>
2020-12-22 17:08:50 +0530

    Solved Raindrops in python

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,16 @@
+def convert(number):
+    result = ""
+
+    if (number % 3) == 0:
+        result = result + "Pling"
+
+    if (number % 5) == 0:
+        result = result + "Plang"
+
+    if (number % 7) == 0:
+        result = result + "Plong"
+
+    if not result:
+        result = "{}".format(number)
+
+    return result
