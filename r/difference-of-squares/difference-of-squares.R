eee8cde19f12f9a6fe83681579e328d9c48a3fba
Raghav Kabra <raghavrkabra@gmail.com>
2020-12-23 20:19:22 +0530

    Added .Rhistory files to .gitignore

--- r/difference-of-squares/difference-of-squares.R
+++ r/difference-of-squares/difference-of-squares.R
@@ -8,7 +8,7 @@ sum_of_n <- function(number) {
 }
 
 
-# this is a stub function that takes a natural_number
+# this function that takes a natural_number
 # and should return the difference-of-squares as described
 # in the README.md
 difference_of_squares <- function(natural_number) {
