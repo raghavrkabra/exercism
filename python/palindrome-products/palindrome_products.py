b84f5059b62d0cde8c118aed10bd63fbb5e64e7d
Raghav Kabra <raghavrkabra@gmail.com>
2020-12-24 21:37:07 +0530

    Written Palindrome Number verification function

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,6 +1,20 @@
-def largest(min_factor, max_factor):
+def largest(min_factor, max_factor: int) -> tuple:
     pass
 
 
-def smallest(min_factor, max_factor):
+def smallest(min_factor, max_factor: int) -> tuple:
     pass
+
+
+def is_palindrome(number: int) -> bool:
+    """Return True if given number is a Palindrome Number"""
+    n = number
+    reverse = 0
+
+    while n > 0:
+        reverse *= 10
+        reverse += n % 10
+        n //= 10
+
+    return number == reverse
+
