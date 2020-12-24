499fc9cfca70d0da2ca780a04a2553638000d02e
Raghav Kabra <raghavrkabra@gmail.com>
2020-12-24 23:27:47 +0530

    Solved Palindrome Product exercise in python track

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,20 +1,67 @@
 def largest(min_factor, max_factor: int) -> tuple:
-    pass
+    """Return the largest palindrome number and its factors in given range"""
+    if min_factor > max_factor:
+        raise ValueError("'min_factor' is grater than 'max_factor'")
+
+    factors = []
+    result = None
+
+    # We are going from higher number to lower number as it will be closer to
+    # the result.
+    for i in range(max_factor, min_factor-1, -1):
+        for j in range(max_factor, i-1, -1):
+            _n = i*j
+
+            if (result is None or _n > result) and is_palindrome(_n):
+                result = _n
+                factors.clear()
+                factors.append([i, j])
+                break
+
+            if _n == result:
+                factors.append([i, j])
+
+            if (result is not None and _n < result):
+                break
+
+    return (result, factors)
 
 
 def smallest(min_factor, max_factor: int) -> tuple:
-    pass
+    """Return the smallest palindrome number and its factors in given range"""
+    if min_factor > max_factor:
+        raise ValueError("'min_factor' is grater than 'max_factor'")
+
+    factors = []
+    result = None
+
+    for i in range(min_factor, max_factor+1):
+        for j in range(i, max_factor+1):
+            _n = i*j
+
+            if (result is None or _n < result) and is_palindrome(_n):
+                result = _n
+                factors.clear()
+                factors.append([i, j])
+                break
+
+            if _n == result:
+                factors.append([i, j])
+
+            if (result is not None and _n > result):
+                break
+
+    return (result, factors)
 
 
 def is_palindrome(number: int) -> bool:
     """Return True if given number is a Palindrome Number"""
-    n = number
+    copy = number
     reverse = 0
 
-    while n > 0:
+    while copy > 0:
         reverse *= 10
-        reverse += n % 10
-        n //= 10
+        reverse += copy % 10
+        copy //= 10
 
     return number == reverse
-
