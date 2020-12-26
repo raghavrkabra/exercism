075378a28fb31239434b1a1f51f2f6e0f730690a
Raghav Kabra <raghavrkabra@gmail.com>
2020-12-26 22:16:15 +0530

    Solved Triangle exercies in python track

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,31 @@
-def equilateral(sides):
-    pass
+def equilateral(sides: list) -> bool:
+    triangle = sorted(sides)
+
+    if triangle[0] == 0:
+        return False
+
+    return len(set(sides)) == 1
 
 
-def isosceles(sides):
-    pass
+def isosceles(sides: list) -> bool:
+    triangle = sorted(sides)
+
+    if triangle[0] == 0:
+        return False
+
+    if triangle[0] == triangle[1] or triangle[1] == triangle[2]:
+        return triangle[0] + triangle[1] >= triangle[2]
+
+    return False
 
 
-def scalene(sides):
-    pass
+def scalene(sides: list) -> bool:
+    triangle = sorted(sides)
+
+    if triangle[0] == 0:
+        return False
+
+    if len(set(triangle)) == 3:
+        return triangle[0] + triangle[1] >= triangle[2]
+
+    return False
