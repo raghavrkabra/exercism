ceec64ebf3b31b9d61f92ac2c0e56cbef143c88f
Raghav Kabra <raghavrkabra@gmail.com>
2020-12-27 22:45:03 +0530

    Solved Collatz Conjecture exercise in python track

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,19 @@
-def steps(number):
-    pass
+def steps(number: int) -> int:
+    """Return the number of steps to reach 1 for Collatz Conjecture"""
+    if number < 1:
+        raise ValueError(f"{number} is not a natural number")
+
+    _n = number
+    count = 0
+
+    while True:
+        if _n % 2 == 0:
+            _n /= 2
+            count += 1
+        elif _n != 1:
+            _n = _n*3 + 1
+            count += 1
+        elif _n == 1:
+            break
+
+    return count
