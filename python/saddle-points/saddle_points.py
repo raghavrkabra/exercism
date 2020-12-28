b088715c74e05c945d05a17b57a0db239f5066e9
Raghav Kabra <raghavrkabra@gmail.com>
2020-12-28 22:57:32 +0530

    Solved Saddle Point exercise in Python track

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,24 @@
 def saddle_points(matrix):
-    pass
+    """Returns the indices of saddle points in given matrix."""
+    if matrix != [] and any(len(row) != len(matrix[0]) for row in matrix):
+        raise ValueError("Irregular matrix")
+
+    points = []
+    indices = [(i,j)
+            for i in range(len(matrix))
+            for j in range(len(matrix[j]))
+            ]
+
+    for i, j in indices:
+        if _is_saddle_point(matrix[i][j], matrix[i], _column(matrix, j)):
+            points.append({"row": i+1, "column": j+1})
+
+    return points
+
+
+def _column(matrix: list, indic: int) -> list:
+    return [row[indic] for row in matrix]
+
+
+def _is_saddle_point(element: int, row: list, column: list) -> bool:
+    return element == max(row) and element == min(column)
