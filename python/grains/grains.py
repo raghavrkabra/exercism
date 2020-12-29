8f23dd1a282619db7146f36b342969a852bc494b
Raghav Kabra <raghavrkabra@gmail.com>
2020-12-29 23:40:00 +0530

    Solved Grains exercise in Python track

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,11 @@
 def square(number):
-    pass
+    """Returns the number of grains on given square on chess board."""
+    if number not in range(1, 65):
+        raise ValueError(f"{number} is not a square in a chess board.")
+
+    return 2**(number - 1)
 
 
 def total():
-    pass
+    """Returns the total number of grains on the chessboard."""
+    return 2**64 - 1
