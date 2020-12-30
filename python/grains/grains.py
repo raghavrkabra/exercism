f82b3444303fd31dd74e8df7e9e820b91a32590f
Raghav Kabra <raghavrkabra@gmail.com>
2020-12-30 17:45:18 +0530

    Update Grains solution for clarity

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,6 @@
 def square(number):
     """Returns the number of grains on given square on chess board."""
-    if number not in range(1, 65):
+    if number not in range(1, 64+1):
         raise ValueError(f"{number} is not a square in a chess board.")
 
     return 2**(number - 1)
