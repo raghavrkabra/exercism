c0ac31c71a999636aa0f158af293ce81fdf8759b
Raghav Kabra <raghavrkabra@gmail.com>
2020-12-30 21:30:19 +0530

    Solve Error Handling exercise in python track

--- python/error-handling/error_handling.py
+++ python/error-handling/error_handling.py
@@ -1,14 +1,27 @@
 def handle_error_by_throwing_exception():
-    pass
+    raise Exception("An exception occurred")
 
 
 def handle_error_by_returning_none(input_data):
-    pass
+    try:
+        return int(input_data)
+    except:
+        return None
 
 
 def handle_error_by_returning_tuple(input_data):
-    pass
+    try:
+        return True, int(input_data)
+    except:
+        return False, None
 
 
 def filelike_objects_are_closed_on_exception(filelike_object):
+    try:
+        filelike_object.open()
+        filelike_object.do_something()
+        filelike_object.close()
+    except:
+        filelike_object.close()
+        raise
     pass
