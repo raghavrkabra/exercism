f9b04590290cd937376b50d591a212954b8c11af
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-01 20:55:51 +0530

    Solve Prime Factors exercise in python track

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,18 @@
+from math import sqrt, floor
+
 def factors(value):
-    pass
+    prime_factors = []
+
+    while value % 2 == 0:
+        prime_factors.append(2)
+        value /= 2
+
+    for d in range(3, floor(sqrt(value)) + 1, 2):
+        while value % d == 0:
+            prime_factors.append(d)
+            value /= d
+
+    if value > 1:
+        prime_factors.append(value)
+
+    return prime_factors
