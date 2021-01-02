b91ac935631577e15478ef33df1e275034ccbdf1
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-02 18:22:10 +0530

    Solve Sum Of Multiple in python track

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,9 @@
-def sum_of_multiples(limit, multiples):
-    pass
+def sum_of_multiples(limit: int, multiples: list) -> int:
+    """Give sum of all the multiples of given numbers untill given limit"""
+    list_of_multiples = [i
+            for i in range(limit)
+            if any(i % factor == 0
+                for factor in multiples if factor != 0
+                )
+            ]
+    return sum(list_of_multiples)
