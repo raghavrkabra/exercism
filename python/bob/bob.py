9407f8122439e5a91ba10c931cd38512365d21b7
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-06 16:52:54 +0530

    Remove unnessery pass and rename some variables

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,4 +1,5 @@
 def response(hey_bob):
+    """How bob respond when you say him someting."""
     hey_bob = hey_bob.strip()
 
     if _is_silence(hey_bob):
@@ -12,23 +13,24 @@ def response(hey_bob):
         return _responses[3]
     else:
         return _responses[4]
-    pass
 
 
-_responses = ["Fine. Be that way!",
-              "Calm down, I know what I'm doing!",
-              "Sure.",
-              "Whoa, chill out!",
-              "Whatever."]
+# Bob has only a few responses which are listed here
+_responses = ["Fine. Be that way!",  # for silence
+              "Calm down, I know what I'm doing!",  # for shouted question
+              "Sure.",  # for question (in normal tone)
+              "Whoa, chill out!",  # for something shouted
+              "Whatever."  # default response
+              ]
 
 
-def _is_question(speech: str) -> str:
-    return speech.endswith("?")
+def _is_question(sentence: str) -> str:
+    return sentence.endswith("?")
 
 
-def _is_shouting(speech: str) -> str:
-    return speech.isupper()
+def _is_shouting(sentence: str) -> str:
+    return sentence.isupper()
 
 
-def _is_silence(speech: str) -> str:
-    return speech == ""
+def _is_silence(sentence: str) -> str:
+    return sentence == ""
