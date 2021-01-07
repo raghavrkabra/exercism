cf7d534df365e041ceea3d7dc31e365659bca5af
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-07 22:44:20 +0530

    Solve Matching Brackets in python track

--- python/matching-brackets/matching_brackets.py
+++ python/matching-brackets/matching_brackets.py
@@ -1,2 +1,12 @@
 def is_paired(input_string):
-    pass
+    brackests = {"(":")", "[":"]", "{":"}"}
+    stack = []
+
+    for char in input_string:
+        if char in brackests:
+            stack.append(brackests[char])
+        elif char in brackests.values():
+            if stack == [] or char != stack.pop():
+                return False
+
+    return stack == []
