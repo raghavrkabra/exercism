da0459eeaf74fa6452a02459f7a86357df24865e
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-09 20:38:11 +0530

    Solve Allergies exercise in python track

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,11 +1,29 @@
+"""Solution to Allergies exercies in python track on exercism."""
+
+
 class Allergies:
+    """This class keep track of allergies of the person."""
 
-    def __init__(self, score):
-        pass
+    _allergies = [
+            "eggs",
+            "peanuts",
+            "shellfish",
+            "strawberries",
+            "tomatoes",
+            "chocolate",
+            "pollen",
+            "cats"
+            ]
 
-    def allergic_to(self, item):
-        pass
+    def __init__(self, score: int) -> None:
+        self.score = score
+
+    def allergic_to(self, item: str) -> bool:
+        """Check to see if a person is allergiec to given item."""
+        return bool(self.score & 1 << self._allergies.index(item))
 
     @property
-    def lst(self):
-        pass
+    def lst(self) -> list:
+        """Turn given allergy code into a list of allergies."""
+        return [allergy for allergy in self._allergies
+                if self.allergic_to(allergy)]
