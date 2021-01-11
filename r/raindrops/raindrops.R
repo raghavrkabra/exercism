4c64db2722f8467aa72dce6e5517fc674b69c92a
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-11 21:56:10 +0530

    Solve Raindrops exercise in R track

--- r/raindrops/raindrops.R
+++ r/raindrops/raindrops.R
@@ -1,3 +1,21 @@
 raindrops <- function(number) {
-  
+  result = ""
+
+  if (number %% 3 == 0) {
+    result <- paste0(result, "Pling");
+  }
+
+  if (number %% 5 == 0) {
+    result <- paste0(result, "Plang");
+  }
+
+  if (number %% 7 == 0) {
+    result <- paste0(result, "Plong");
+  }
+
+  if (result == "") {
+    result = toString(number)
+  }
+
+  return(result)
 }
