c5bd4cff5f749c850c3eb8083f3f3c333c798bb9
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-11 11:54:40 +0530

    Solve Space Age exercise in python track

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,3 +1,25 @@
+"""Solution to Space Age exercise in python track."""
+
+
+def _years_on_planet(year_length):
+    def no_of_years(self):
+        return round(self.seconds / year_length, 2)
+
+    return no_of_years
+
+
 class SpaceAge:
+    """This class convert given seconds to years on different planets."""
+
     def __init__(self, seconds):
-        pass
+        self.seconds = seconds
+        self._earth_year = 31557600
+
+    on_earth = _years_on_planet(31557600)
+    on_mercury = _years_on_planet(7600544)
+    on_venus = _years_on_planet(19414149)
+    on_mars = _years_on_planet(59354033)
+    on_jupiter = _years_on_planet(374355659)
+    on_saturn = _years_on_planet(929292362)
+    on_uranus = _years_on_planet(2651370019)
+    on_neptune = _years_on_planet(5200418560)
