de7e4f52b215cff6f098f89708c9d90f37f9c3d4
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-12 23:17:00 +0530

    Implament Ceaser Cipher and some code cleanup

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -10,28 +10,30 @@ class Cipher:
 
     def __init__(self, key=None):
         if key is None:
-            key = "".join(secrets.choice(letters) for i in range(100))
+            key = "".join(secrets.choice(letters) for _ in range(100))
 
         self.key = key
 
     def encode(self, text):
         """Encode plaintext massage to cipher."""
-        msg = []
-        for c, k in zip(text, cycle(self.key)):
-            letter = ((ord(c) + ord(k) - 2*ord(letters[0])) % len(letters) +
-                       ord(letters[0]))
-            msg.append(chr(letter))
+        encoded = []
+        for char, k in zip(text, cycle(self.key)):
+            letter = ((ord(char) + ord(k) - 2*ord(letters[0])) % len(letters) +
+                      ord(letters[0]))
+            encoded.append(chr(letter))
 
-        return "".join(msg)
+        return "".join(encoded)
 
     def decode(self, text):
         """Decode Cipher to plaintext massage."""
-        msg = []
-        for c, k in zip(text, cycle(self.key)):
+        decoded = []
+        for char, k in zip(text, cycle(self.key)):
             # python ensure that moduler division always return a positive
             # number, so no need to worry about it.
-            letter = ((ord(c) - ord(k)) % len(letters) +
-                       ord(letters[0]))
-            msg.append(chr(letter))
+            letter = ((ord(char) - ord(k)) % len(letters) +
+                      ord(letters[0]))
+            decoded.append(chr(letter))
 
-        return "".join(msg)
+        return "".join(decoded)
+
+Ceaser = Cipher("d")
