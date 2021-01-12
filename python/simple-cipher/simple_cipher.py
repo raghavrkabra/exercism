5d4bf0721b01c7a25f5d44a9e3037274037a22f1
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-12 23:10:22 +0530

    Remove uppercase code.

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -2,8 +2,7 @@
 
 from itertools import cycle
 import secrets
-from string import (ascii_lowercase as lowercase,
-                    ascii_uppercase as uppercase)
+from string import ascii_lowercase as letters
 
 
 class Cipher:
@@ -11,7 +10,7 @@ class Cipher:
 
     def __init__(self, key=None):
         if key is None:
-            key = "".join(secrets.choice(lowercase) for i in range(100))
+            key = "".join(secrets.choice(letters) for i in range(100))
 
         self.key = key
 
@@ -19,17 +18,8 @@ class Cipher:
         """Encode plaintext massage to cipher."""
         msg = []
         for c, k in zip(text, cycle(self.key)):
-            if c.islower():
-                letter = ((ord(c) + ord(k) - 2*ord(lowercase[0])) % 26 +
-                           ord(lowercase[0]))
-            if c.isupper():
-                # This is bad and should not be done in actual code
-                # I am only doing this because I Want to encode and decode
-                # uppercase letters too but don't want to spend too much time
-                # on this exercise
-                k.upper()
-                letter = ((ord(c) + ord(k) - 2*ord(uppercase[0])) % 26 +
-                           ord(uppercase[0]))
+            letter = ((ord(c) + ord(k) - 2*ord(letters[0])) % len(letters) +
+                       ord(letters[0]))
             msg.append(chr(letter))
 
         return "".join(msg)
@@ -38,19 +28,10 @@ class Cipher:
         """Decode Cipher to plaintext massage."""
         msg = []
         for c, k in zip(text, cycle(self.key)):
-            if c.islower():
-                # python ensure that moduler division always return a positive
-                # number, so no need to worry about it.
-                letter = ((ord(c) - ord(k)) % 26 +
-                           ord(lowercase[0]))
-            if c.isupper():
-                # This is bad and should not be done in actual code
-                # I am only doing this because I Want to encode and decode
-                # uppercase letters too but don't want to spend too much time
-                # on this exercise
-                k.upper()
-                letter = ((ord(c) - ord(k)) % 26 +
-                           ord(uppercase[0]))
+            # python ensure that moduler division always return a positive
+            # number, so no need to worry about it.
+            letter = ((ord(c) - ord(k)) % len(letters) +
+                       ord(letters[0]))
             msg.append(chr(letter))
 
         return "".join(msg)
