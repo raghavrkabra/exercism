5ee33a968d9b82b778cd4db907ebd54ec3e70dd4
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-14 13:35:45 +0530

    Solve Word Count exercise in python track

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,14 @@
+import re
+
 def count_words(sentence):
-    pass
+    pattern = re.compile("[a-z]+'[a-z]+|[a-z0-9]+")
+    word_list = re.findall(pattern, sentence.lower())
+
+    counter = {}
+    for w in word_list:
+        if w in counter:
+            counter[w] += 1
+        else:
+            counter[w] = 1
+
+    return counter
