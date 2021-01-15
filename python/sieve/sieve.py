9a7fa75be7748f47ba74592c94faa78baab01d77
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-15 22:26:55 +0530

    Solve Sieve exercise in Python track

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,2 +1,15 @@
-def primes(limit):
-    pass
+"""Solution to Sieve exercise."""
+
+from typing import List
+
+
+def primes(limit: int) -> List[int]:
+    """Return all the prime numbers smaller then or equal to limit."""
+    sieve = dict.fromkeys(range(2, limit+1), True)
+
+    for key in iter(sieve):
+        if sieve[key] is True:
+            for i in range(pow(key, 2), limit+1, key):
+                sieve[i] = False
+
+    return [key for key in sieve if sieve[key] is True]
