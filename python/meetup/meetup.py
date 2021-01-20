9aa76924d4d1b1bbc4659bcc6816a44248a7c1b7
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-20 22:49:10 +0530

    Solve Meetup exercise in python track
    MeetupDayException bug fix
    PEP-8 and PEP-257 related changes
    Refactor code

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -1,21 +1,26 @@
-import datetime
+"""Solution to Meetup exercise in python track."""
+
 from calendar import Calendar
 
 
 def meetup(year: int, month: int, week: str, day_of_week: str):
-    cal = Calendar()
-    days = [day for day in cal.itermonthdates(year, month)
+    """Return the date of meetup."""
+    days = [day for day in Calendar().itermonthdates(year, month)
             if day.strftime("%A") == day_of_week and day.month == month]
-    weeks = {"1st":0, "2nd":1, "3rd":2, "4th":3, "5th":4, "last":-1}
+    weeks = {"1st": 0, "2nd": 1, "3rd": 2, "4th": 3, "5th": 4, "last": -1}
 
     if week == "teenth":
         for day in days:
-            if day.day in range(13, 19+1):
+            if day.day in range(13, 19+1):  # -teen days
                 return day
+
     try:
         return days[weeks[week]]
     except IndexError:
-        raise MeetupDayException("day does not exist")
+        raise MeetupDayException("No such day exists.")
 
-def MeetupDayException(Exception):
+
+class MeetupDayException(Exception):
+    """MeetupDayException for incorrect parameters in meetup function."""
+
     pass
