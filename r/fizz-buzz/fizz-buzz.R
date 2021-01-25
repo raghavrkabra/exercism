fd9b07017d0b62b70aa560e497ac56c38b1be561
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-25 16:05:10 +0530

    Solve Fizz Buzz exercise in R track

--- r/fizz-buzz/fizz-buzz.R
+++ r/fizz-buzz/fizz-buzz.R
@@ -1,2 +1,18 @@
 fizz_buzz <- function(input) {
+  series <- c(1:input)
+  for (i in series ) {
+    if (i %% 3 == 0 && i %% 5 == 0) {
+      series[i] <- 'Fizz Buzz';
+    }
+    else if (i %% 5 == 0) {
+      series[i] <- 'Buzz';
+    }
+    else if (i %% 3 == 0) {
+      series[i] <- 'Fizz';
+    }
+    else {
+      series[i] <- toString(i);
+    }
+  }
+  return(series);
 }
