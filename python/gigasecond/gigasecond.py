37dc9ed2ff1b1f9488a2b9da1116709f7fe01eeb
Raghav Kabra <raghavrkabra@gmail.com>
2021-01-27 21:26:55 +0530

    Solve Gigasecond exercise in python Track

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,2 +1,8 @@
+"""Solution to Gigasecond exercise on exercism."""
+from datetime import timedelta
+
+
 def add(moment):
-    pass
+    """Return the moment that is a gigasecond after given moment."""
+    gigaseconds = timedelta(seconds=1E9)  # 1E9 == 1_000_000_000
+    return moment + gigaseconds
