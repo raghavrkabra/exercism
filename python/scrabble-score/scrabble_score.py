04af68f4b361a57d775255f9538f18a913530fd7
Raghav Kabra <raghavrkabra@gmail.com>
2021-02-13 18:51:09 +0530

    Solve Scrabble Score exercise in Python Track.

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,19 @@
+"""Solution to Scrabble Score exercise in python Track on Exercism."""
+
+
 def score(word):
-    pass
+    """Calculate Scrabble score of given word."""
+    latter_values = {
+                     **dict.fromkeys((latter for latter in "AEIOULNRST"), 1),
+                     **dict.fromkeys((latter for latter in "DG"), 2),
+                     **dict.fromkeys((latter for latter in "BCMP"), 3),
+                     **dict.fromkeys((latter for latter in "FHVWY"), 4),
+                     **dict.fromkeys((latter for latter in "K"), 5),
+                     **dict.fromkeys((latter for latter in "JX"), 8),
+                     **dict.fromkeys((latter for latter in "QZ"), 10),
+                    }
+    score_ = 0
+    for latter in word.upper():
+        score_ += latter_values.get(latter)
+
+    return score_
