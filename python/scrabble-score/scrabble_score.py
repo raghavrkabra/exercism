be5eb5bad6a74f9bbe4d17f136f277177eb3a76e
Raghav Kabra <raghavrkabra@gmail.com>
2021-03-11 16:52:11 +0530

    Update Scrabble Score exercise in python track for readability

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -4,16 +4,13 @@
 def score(word):
     """Calculate Scrabble score of given word."""
     letter_values = {
-                     **dict.fromkeys((letter for letter in "AEIOULNRST"), 1),
-                     **dict.fromkeys((letter for letter in "DG"), 2),
-                     **dict.fromkeys((letter for letter in "BCMP"), 3),
-                     **dict.fromkeys((letter for letter in "FHVWY"), 4),
-                     **dict.fromkeys((letter for letter in "K"), 5),
-                     **dict.fromkeys((letter for letter in "JX"), 8),
-                     **dict.fromkeys((letter for letter in "QZ"), 10),
+                     **dict.fromkeys(iter("AEIOULNRST"), 1),
+                     **dict.fromkeys(iter("DG"), 2),
+                     **dict.fromkeys(iter("BCMP"), 3),
+                     **dict.fromkeys(iter("FHVWY"), 4),
+                     **dict.fromkeys(iter("K"), 5),
+                     **dict.fromkeys(iter("JX"), 8),
+                     **dict.fromkeys(iter("QZ"), 10),
                     }
-    score_ = 0
-    for letter in word.upper():
-        score_ += letter_values.get(letter)
 
-    return score_
+    return sum(letter_values.get(letter) for letter in word.upper())
