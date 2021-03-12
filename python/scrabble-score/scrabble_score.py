8dbad12befeccad29347c6d863fa34cc60d25f12
Raghav Kabra <raghavrkabra@gmail.com>
2021-03-12 16:44:23 +0530

    Farther simplication in Scrabble Score exercise

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -4,13 +4,13 @@
 def score(word):
     """Calculate Scrabble score of given word."""
     letter_values = {
-                     **dict.fromkeys(iter("AEIOULNRST"), 1),
-                     **dict.fromkeys(iter("DG"), 2),
-                     **dict.fromkeys(iter("BCMP"), 3),
-                     **dict.fromkeys(iter("FHVWY"), 4),
-                     **dict.fromkeys(iter("K"), 5),
-                     **dict.fromkeys(iter("JX"), 8),
-                     **dict.fromkeys(iter("QZ"), 10),
-                    }
+                     **dict.fromkeys("AEIOULNRST", 1),
+                     **dict.fromkeys("DG", 2),
+                     **dict.fromkeys("BCMP", 3),
+                     **dict.fromkeys("FHVWY", 4),
+                     **dict.fromkeys("K", 5),
+                     **dict.fromkeys("JX", 8),
+                     **dict.fromkeys("QZ", 10),
+    }
 
-    return sum(letter_values.get(letter) for letter in word.upper())
+    return sum(letter_values[letter] for letter in word.upper())
