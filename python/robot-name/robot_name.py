2912c4ddde2d1a918115d5c918b7b116418a02e0
Raghav Kabra <raghavrkabra@gmail.com>
2021-04-04 16:40:58 +0530

    Code restructure and bug fix in Robot Name exercise

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -2,27 +2,26 @@ from random import choices
 from string import ascii_uppercase, digits
 
 
-def _generate_name():
-    """Create a valid robot name."""
-    return "".join(choices(ascii_uppercase, k=2) + choices(digits, k=3))
-
 class Robot:
     """Robot factory to create and reset them."""
-    def __init__(self):
-        self.name = self.create_robot()
 
     robot_population = set()
 
+    def __init__(self):
+        self.name = self.create_robot()
+
+    def _generate_name(self):
+        """Create a valid robot name."""
+        return "".join(choices(ascii_uppercase, k=2) + choices(digits, k=3))
+
     def create_robot(self):
         """Create a robot and ensure it's name is unique."""
-        name_ = _generate_name()
-        while name_ in self.robot_population:
-            name_ = _generate_name()
-        self.robot_population.add(name_)
-        return name_
+        name = self._generate_name()
+        while name in self.robot_population:
+            name = self._generate_name()
+        self.robot_population.add(name)
+        return name
 
     def reset(self):
         """Reset a robot and give it a new new name."""
-        old_name = self.name
         self.name = self.create_robot()
-        self.robot_population.remove(old_name)
